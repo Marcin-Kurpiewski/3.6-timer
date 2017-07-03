1395a03567f71dbfe6a74fa807361bd9702190fd
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-07-03 13:00:11 +0200

    add babel, pc.jason

---
+++ script-compiled.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * Created by HP-M on 03.07.2017.
+ */
+
+var sayHello = function sayHello() {
+  return alert('Hello world!');
+};
+sayHello();
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 /**
  * Created by HP-M on 03.07.2017.
  */
+
+const sayHello = () => alert('Hello world!');
+sayHello();
